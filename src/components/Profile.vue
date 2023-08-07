3840451f3b93000c70eaeba01e78b544ffa5222f
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2023-08-07 18:21:31 +0500

    api fix

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -101,7 +101,7 @@ const hideEdit = () => {
 };
 
 /* Fetch Data */
-const url = 'http://api.pharmiq.uz/api/v1-1/mobile-user';
+const url = 'https://api.pharmiq.uz/api/v1-1/mobile-user';
 const apiKey = '10638|kw7JpzOBtw1Hig3w2bkCEsboQxS8FT9XDK4UcPbB';
 const headers = {
 	Accept: 'application/json',
