8f86cbb25873d92ca40c5d27046191d61f351a11
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2023-08-08 20:47:59 +0500

    https

--- src/env.js
+++ src/env.js
@@ -1,8 +1,8 @@
 const env = {
 	apikey: '10638|kw7JpzOBtw1Hig3w2bkCEsboQxS8FT9XDK4UcPbB',
-	newsUrl: 'http://api.pharmiq.uz/api/v1-1/spa-news?page=1',
-	leaderboardsUrl: 'http://api.pharmiq.uz/api/v1-1/spa-users/users-top-iqc-statistics',
+	newsUrl: 'https://api.pharmiq.uz/api/v1-1/spa-news?page=1',
+	leaderboardsUrl: 'https://api.pharmiq.uz/api/v1-1/spa-users/users-top-iqc-statistics',
 	userUrl: 'https://api.pharmiq.uz/api/v1-1/mobile-user',
-	promocodeUrl: 'http://api.pharmiq.uz/api/v1-1/spa-users/user-promocode',
+	promocodeUrl: 'https://api.pharmiq.uz/api/v1-1/spa-users/user-promocode',
 };
 export default env;
