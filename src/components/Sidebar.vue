dfe9a503157313f20b99f9362f3c04c40e1433fc
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2023-08-10 22:26:07 +0500

    darkmode

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -21,35 +21,28 @@
 			</RouterLink>
 			<input type="checkbox" name="darkmode-toggle" id="darkmode-toggle" />
 			<label @click="changeTheme" for="darkmode-toggle">
-				<svg
-					class="sun"
-					width="800px"
-					height="800px"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
+				<svg class="sun" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 					<path
 						d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z"
-						stroke="#000000"
+						stroke="yellow"
 						stroke-width="2"
 						stroke-linecap="round"
 						stroke-linejoin="round"
 					/>
 				</svg>
-				<svg
-					class="moon"
-					width="800px"
-					height="800px"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
+				<svg class="moon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 					<path
-						d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z"
-						stroke="#000000"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					/>
+						d="M19.9001 2.30719C19.7392 1.8976 19.1616 1.8976 19.0007 2.30719L18.5703 3.40247C18.5212 3.52752 18.4226 3.62651 18.298 3.67583L17.2067 4.1078C16.7986 4.26934 16.7986 4.849 17.2067 5.01054L18.298 5.44252C18.4226 5.49184 18.5212 5.59082 18.5703 5.71587L19.0007 6.81115C19.1616 7.22074 19.7392 7.22074 19.9001 6.81116L20.3305 5.71587C20.3796 5.59082 20.4782 5.49184 20.6028 5.44252L21.6941 5.01054C22.1022 4.849 22.1022 4.26934 21.6941 4.1078L20.6028 3.67583C20.4782 3.62651 20.3796 3.52752 20.3305 3.40247L19.9001 2.30719Z"
+						fill="#ffffff"
+					></path>
+					<path
+						d="M16.0328 8.12967C15.8718 7.72009 15.2943 7.72009 15.1333 8.12967L14.9764 8.52902C14.9273 8.65407 14.8287 8.75305 14.7041 8.80237L14.3062 8.95987C13.8981 9.12141 13.8981 9.70107 14.3062 9.86261L14.7041 10.0201C14.8287 10.0694 14.9273 10.1684 14.9764 10.2935L15.1333 10.6928C15.2943 11.1024 15.8718 11.1024 16.0328 10.6928L16.1897 10.2935C16.2388 10.1684 16.3374 10.0694 16.462 10.0201L16.8599 9.86261C17.268 9.70107 17.268 9.12141 16.8599 8.95987L16.462 8.80237C16.3374 8.75305 16.2388 8.65407 16.1897 8.52902L16.0328 8.12967Z"
+						fill="#ffffff"
+					></path>
+					<path
+						d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+						fill="#ffffff"
+					></path>
 				</svg>
 			</label>
 		</nav>
@@ -170,20 +163,20 @@ label {
 	position: relative;
 	display: block;
 	padding: 0.3rem;
-	background: #ebebeb;
+	background: rgb(124, 202, 250);
 	border-radius: 10rem;
 	box-shadow: inset 0px 5px 15px rgba(0, 0, 0, 0.4), inset 0px -5px 15px rgba(255, 255, 255, 0.4);
 	cursor: pointer;
 	&::after {
 		content: '';
-		width: 2.8rem;
+		width: 2.6rem;
 		height: 80%;
 		position: absolute;
 		left: 6px;
-		background: linear-gradient(180deg, #ffcc89, #d8860b);
+		background: white;
 		border-radius: 10rem;
 		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
-		transition: all 0.3s;
+		transition: all 0.6s;
 	}
 	&:active::after {
 		width: 3.4rem;
@@ -192,16 +185,15 @@ label {
 		position: absolute;
 		width: 3.2rem;
 		height: 80%;
-		z-index: 2;
+		transition: all 0.6s;
 		&.sun {
-			left: 3px;
+			right: 3px;
 			fill: #fff176;
-			transition: all 0.3s;
 		}
 		&.moon {
-			right: 3px;
-			fill: #7e7e7e;
-			transition: all 0.3s;
+			transform: scale(0.7) translateX(6px);
+			left: 3px;
+			fill: #fff;
 		}
 	}
 }
@@ -215,13 +207,12 @@ label {
 		&::after {
 			left: 7.4rem;
 			transform: translateX(-100%);
-			background: linear-gradient(180deg, #777, #3a3a3a);
 		}
 		& svg.sun {
-			fill: #7e7e7e;
+			transform: scale(0.7) translateX(-6px);
 		}
 		& svg.moon {
-			fill: var(--color-primary);
+			transform: scale(1);
 		}
 	}
 }
